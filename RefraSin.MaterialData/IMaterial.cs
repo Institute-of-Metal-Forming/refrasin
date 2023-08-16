48bf317ff8186342c1c75a6c0e7f70865ba8db0e
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-08-16 13:13:23 +0200

    implement basic material data stuff

--- RefraSin.MaterialData/IMaterial.cs
+++ RefraSin.MaterialData/IMaterial.cs
@@ -1,22 +1,52 @@
 namespace RefraSin.MaterialData;
 
+/// <summary>
+/// Interface for types providing material data.
+/// </summary>
 public interface IMaterial
 {
+    /// <summary>
+    /// Unique id.
+    /// </summary>
     Guid Id { get; }
 
+    /// <summary>
+    /// Human readable name.
+    /// </summary>
     string Name { get; }
 
+    /// <summary>
+    /// Diffusion coefficient at free surfaces.
+    /// </summary>
     double SurfaceDiffusionCoefficient { get; }
 
+    /// <summary>
+    /// Diffusion coefficient in bulk material.
+    /// </summary>
     double BulkDiffusionCoefficient { get; }
 
+    /// <summary>
+    /// Vacancy concentration in equilibrium without effecting stresses.
+    /// </summary>
     double EquilibriumVacancyConcentration { get; }
 
+    /// <summary>
+    /// Interface energy of free surfaces.
+    /// </summary>
     double SurfaceEnergy { get; }
 
+    /// <summary>
+    /// Molar volume.
+    /// </summary>
     double MolarVolume { get; }
 
+    /// <summary>
+    /// Density.
+    /// </summary>
     double Density { get; }
 
-    double MolarWeight { get; set; }
+    /// <summary>
+    /// Molar weight.
+    /// </summary>
+    double MolarWeight { get; }
 }
---
+++ RefraSin.MaterialData/MaterialRegistry.cs
@@ -0,0 +1,36 @@
+namespace RefraSin.MaterialData;
+
+/// <summary>
+/// A simple in-memory material registry using backing dictionaries.
+/// </summary>
+public record MaterialRegistry : IMaterialRegistry
+{
+    private Dictionary<Guid, IMaterial> _materials = new();
+    private Dictionary<(Guid From, Guid To), IMaterialInterface> _interfaces = new();
+
+    /// <inheritdoc />
+    public IReadOnlyList<IMaterial> Materials => _materials.Values.ToArray();
+
+    /// <inheritdoc />
+    public IReadOnlyList<IMaterialInterface> MaterialInterfaces => _interfaces.Values.ToArray();
+
+    /// <inheritdoc />
+    public void RegisterMaterial(IMaterial material) => _materials.Add(material.Id, material);
+
+    /// <inheritdoc />
+    public IMaterial GetMaterial(Guid id) => _materials[id];
+
+    /// <inheritdoc />
+    public IMaterial GetMaterial(string name) => _materials.Values.First(m => m.Name == name);
+
+    /// <inheritdoc />
+    public void RegisterMaterialInterface(IMaterialInterface materialInterface) =>
+        _interfaces.Add((materialInterface.From, materialInterface.To), materialInterface);
+
+    /// <inheritdoc />
+    public IMaterialInterface GetMaterialInterface(Guid from, Guid to) => _interfaces[(from, to)];
+
+    /// <inheritdoc />
+    public IMaterialInterface GetMaterialInterface(string from, string to) =>
+        _interfaces.Values.First(i => _materials[i.From].Name == from && _materials[i.To].Name == to);
+}
