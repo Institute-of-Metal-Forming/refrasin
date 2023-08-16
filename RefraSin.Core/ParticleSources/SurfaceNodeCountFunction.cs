7b7f233283cf69714f4c22f237f4387e72f6d176
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-08-16 17:13:20 +0200

    remove old projects

--- RefraSin.Core/ParticleSources/SurfaceNodeCountFunction.cs
+++
@@ -1,7 +0,0 @@
-namespace RefraSin.Core.ParticleSources
-{
-    /// <summary>
-    /// Delegate for functions that determine the surface knot count from the particle radius.
-    /// </summary>
-    public delegate int SurfaceNodeCountFunction (double particleRadius);
-}
