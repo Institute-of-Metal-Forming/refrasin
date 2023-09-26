fb7855a1bda71997079cbcefd323b044e9e2b5c9
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-09-26 16:04:07 +0200

    create test project

---
+++ RefraSin.TEPSolver.Test/OneParticleTest.cs
@@ -0,0 +1,14 @@
+namespace RefraSin.TEPSolver.Test;
+
+[TestFixture]
+public class OneParticleTest
+{
+    [SetUp]
+    public void Setup() { }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
