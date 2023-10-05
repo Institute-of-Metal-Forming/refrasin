a41e92eeb2bb6901d12a4b95949b3b1e42a2ca35
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-10-05 17:38:06 +0200

    correct units in test

--- RefraSin.TEPSolver.Test/OneParticleTest.cs
+++ RefraSin.TEPSolver.Test/OneParticleTest.cs
@@ -16,7 +16,7 @@ public class OneParticleTest
     [SetUp]
     public void Setup()
     {
-        _particleSpec = new ShapeFunctionParticleSpecFactory(100, 0.1, 5, 0.1, Guid.NewGuid()).GetParticleSpec();
+        _particleSpec = new ShapeFunctionParticleSpecFactory(100e-6, 0.1, 5, 0.1, Guid.NewGuid()).GetParticleSpec();
         _solver = new Solver
         {
             LoggerFactory = LoggerFactory.Create(builder => { builder.AddConsole(); }),
@@ -29,10 +29,10 @@ public class OneParticleTest
         _material = new Material(
             _particleSpec.MaterialId,
             "Al2O3",
-            1e-9,
+            1.65e-10,
             0,
             1e-4,
-            1,
+            0.9,
             1.8e3,
             101.96e-3
         );
@@ -41,7 +41,7 @@ public class OneParticleTest
             _material.Id,
             _material.Id,
             0.5,
-            1e-9,
+            1.65e-10,
             0
         );
 
@@ -51,7 +51,7 @@ public class OneParticleTest
             new[] { _particleSpec },
             new[] { _material },
             new[] { _materialInterface },
-            1273
+            2073
         );
     }
 
