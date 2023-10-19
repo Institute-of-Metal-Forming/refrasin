5448e23318a6d591573ea6ba14422a362dc82a5a
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-10-19 13:55:22 +0200

    ext test analysis

--- RefraSin.TEPSolver.Test/OneParticleTest.cs
+++ RefraSin.TEPSolver.Test/OneParticleTest.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using MathNet.Numerics.Statistics;
 using Microsoft.Extensions.Logging;
 using static MoreLinq.Extensions.IndexExtension;
 using RefraSin.MaterialData;
@@ -7,6 +8,7 @@ using RefraSin.ParticleModel.ParticleSpecFactories;
 using RefraSin.ProcessModel;
 using RefraSin.Storage;
 using ScottPlot;
+using Serilog;
 using static NUnit.Framework.Assert;
 using Particle = RefraSin.TEPSolver.ParticleModel.Particle;
 
@@ -23,9 +25,15 @@ public class OneParticleTest
         _particleSpec = new ShapeFunctionParticleSpecFactory(100e-6, 0.1, 5, 0.1, Guid.NewGuid()).GetParticleSpec();
         _solutionStorage = new InMemorySolutionStorage();
 
+        _tempDir = Path.GetTempFileName().Replace(".tmp", "");
+        Directory.CreateDirectory(_tempDir);
+        TestContext.WriteLine(_tempDir);
+
+        var loggerFactory = LoggerFactory.Create(builder => { builder.AddFile(Path.Combine(_tempDir, "test.log")); });
+
         _solver = new Solver
         {
-            LoggerFactory = LoggerFactory.Create(builder => { builder.AddConsole(); }),
+            LoggerFactory = loggerFactory,
             Options = new SolverOptions
             {
                 InitialTimeStepWidth = 1,
@@ -71,6 +79,7 @@ public class OneParticleTest
     private IMaterialInterface _materialInterface;
     private ISinteringProcess _process;
     private InMemorySolutionStorage _solutionStorage;
+    private string _tempDir;
 
     [Test]
     public void TestCreateSession()
@@ -93,46 +102,65 @@ public class OneParticleTest
         }
         finally
         {
-            var dir = Path.GetTempFileName().Replace(".tmp", "");
-            Directory.CreateDirectory(dir);
-            TestContext.WriteLine(dir);
-
-            var pdir = Path.Combine(dir, "p");
-            Directory.CreateDirectory(pdir);
-
-            foreach (var (i, state) in _solutionStorage.States.Index())
-            {
-                var plt = new Plot();
-
-                var coordinates = state.ParticleStates[0].Nodes
-                    .Append(state.ParticleStates[0].Nodes[0])
-                    .Select(n => new ScottPlot.Coordinates(n.AbsoluteCoordinates.X, n.AbsoluteCoordinates.Y))
-                    .ToArray();
-                plt.Add.Scatter(coordinates);
-
-                plt.Title($"t = {state.Time.ToString(CultureInfo.InvariantCulture)}");
-
-                plt.SavePng(Path.Combine(pdir, $"{i}.png"), 3000, 3000);
-            }
-
-            var nddir = Path.Combine(dir, "nd");
-            Directory.CreateDirectory(nddir);
-
-            foreach (var (i, step) in _solutionStorage.Steps.Index())
-            {
-                var plt = new Plot();
-
-                var coordinates = step.ParticleTimeSteps[0].NodeTimeSteps.Values
-                    .Select((n, j) => new ScottPlot.Coordinates(j, n.NormalDisplacement))
-                    .ToArray();
-                plt.Add.Scatter(coordinates);
-
-                plt.Add.Line(0, 0, coordinates.Length, 0);
-
-                plt.Title($"t = {step.StartTime.ToString(CultureInfo.InvariantCulture)} - {step.EndTime.ToString(CultureInfo.InvariantCulture)}");
-
-                plt.SavePng(Path.Combine(nddir, $"{i}.png"), 600, 400);
-            }
+            PlotParticles();
+            PlotDisplacements();
+            PlotTimeSteps();
         }
+    }
+
+    private void PlotParticles()
+    {
+        var dir = Path.Combine(_tempDir, "p");
+        Directory.CreateDirectory(dir);
+
+        foreach (var (i, state) in _solutionStorage.States.Index())
+        {
+            var plt = new Plot();
+
+            var coordinates = state.ParticleStates[0].Nodes
+                .Append(state.ParticleStates[0].Nodes[0])
+                .Select(n => new ScottPlot.Coordinates(n.AbsoluteCoordinates.X, n.AbsoluteCoordinates.Y))
+                .ToArray();
+            plt.Add.Scatter(coordinates);
+
+            plt.Title($"t = {state.Time.ToString(CultureInfo.InvariantCulture)}");
+
+            plt.SavePng(Path.Combine(dir, $"{i}.png"), 3000, 3000);
+        }
+    }
+
+    private void PlotDisplacements()
+    {
+        var dir = Path.Combine(_tempDir, "nd");
+        Directory.CreateDirectory(dir);
+
+        foreach (var (i, step) in _solutionStorage.Steps.Index())
+        {
+            var plt = new Plot();
+
+            var coordinates = step.ParticleTimeSteps[0].NodeTimeSteps.Values
+                .Select((n, j) => new ScottPlot.Coordinates(j, n.NormalDisplacement))
+                .ToArray();
+            plt.Add.Scatter(coordinates);
+
+            plt.Add.Line(0, 0, coordinates.Length, 0);
+
+            plt.Title($"t = {step.StartTime.ToString(CultureInfo.InvariantCulture)} - {step.EndTime.ToString(CultureInfo.InvariantCulture)}");
+
+            plt.SavePng(Path.Combine(dir, $"{i}.png"), 600, 400);
+        }
+    }
+
+    private void PlotTimeSteps()
+    {
+        var plt = new Plot();
+
+        var steps = _solutionStorage.Steps.Select(s => new ScottPlot.Coordinates(s.StartTime, s.TimeStepWidth)).ToArray();
+        plt.Add.Scatter(steps);
+
+        var meanStepWidth = steps.Select(s => s.Y).Mean();
+        plt.Add.Line(0, meanStepWidth, _process.EndTime, meanStepWidth);
+
+        plt.SavePng(Path.Combine(_tempDir, "timeSteps.png"), 600, 400);
     }
 }
