31392725a853a11c076d8610afc4581f1cfefcad
Max Weiner <max.weiner@posteo.de>
2023-12-12 09:38:43 +0100

    do not create new solution states

--- RefraSin.Storage/InMemorySolutionStorage.cs
+++ RefraSin.Storage/InMemorySolutionStorage.cs
@@ -5,32 +5,26 @@ public class InMemorySolutionStorage : ISolutionStorage
     /// <summary>
     /// List of all stored solution states.
     /// </summary>
-    public IReadOnlyList<SolutionState> States => _states;
+    public IReadOnlyList<ISolutionState> States => _states;
 
-    private readonly List<SolutionState> _states = new();
+    private readonly List<ISolutionState> _states = new();
 
     /// <summary>
     /// List of all stored solution steps.
     /// </summary>
-    public IReadOnlyList<SolutionStep> Steps => _steps;
+    public IReadOnlyList<ISolutionStep> Steps => _steps;
 
-    private readonly List<SolutionStep> _steps = new();
+    private readonly List<ISolutionStep> _steps = new();
 
     /// <inheritdoc />
     public void StoreState(ISolutionState state)
     {
-        if (state is SolutionState s)
-            _states.Add(s);
-        else
-            _states.Add(new SolutionState(state));
+        _states.Add(state);
     }
 
     /// <inheritdoc />
     public void StoreStep(ISolutionStep step)
     {
-        if (step is SolutionStep s)
-            _steps.Add(s);
-        else
-            _steps.Add(new SolutionStep(step));
+        _steps.Add(step);
     }
 }
