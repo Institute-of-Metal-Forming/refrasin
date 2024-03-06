5ffa728af0835162b198338f1542a8c59b3e88d5
Max Weiner <max.weiner@posteo.de>
2024-03-06 11:29:04 +0100

    remove helper function

--- RefraSin.TEPSolver/EquationSystem/Helper.cs
+++ RefraSin.TEPSolver/EquationSystem/Helper.cs
@@ -7,7 +7,4 @@ public static class Helper
 {
     public static IEnumerable<T> Join<T>(params IEnumerable<T>[] equations) =>
         equations.SelectMany(e => e);
-
-    public static Vector<double> ToBorderRowVector(this IEnumerable<(int colIndex, double value)> indexedValues, StepVector stepVector) =>
-        Vector<double>.Build.SparseOfIndexed(stepVector.StepVectorMap.BorderLength, indexedValues);
 }
