132db2e9c2d53cade34ddd971a97d6cdbb8fd0d4
Max Weiner <max.weiner@posteo.de>
2025-02-28 17:18:45 +0100

    store absolute node position in parquet

--- RefraSin.ParquetStorage/PolarPointData.cs
+++ RefraSin.ParquetStorage/PolarPointData.cs
@@ -8,5 +8,15 @@ public class PolarPointData
     public double Phi { get; set; }
 
     public static PolarPointData From(IPolarPoint from) =>
-        new PolarPointData() { R = from.R, Phi = from.Phi };
+        new PolarPointData()
+        {
+            R = from.R,
+            Phi = from.Phi,
+            X = from.Absolute.X,
+            Y = from.Absolute.Y,
+        };
+
+    public double X { get; set; }
+
+    public double Y { get; set; }
 }
