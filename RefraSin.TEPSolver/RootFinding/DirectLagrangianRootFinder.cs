64085232cc218cc3d9279a2e15cc2e125ac98a94
Max Weiner <max.weiner@posteo.de>
2025-05-06 16:37:15 +0200

    randomize trivial solution avoidance scaling

--- RefraSin.TEPSolver/RootFinding/DirectLagrangianRootFinder.cs
+++ RefraSin.TEPSolver/RootFinding/DirectLagrangianRootFinder.cs
@@ -1,5 +1,6 @@
 using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.Optimization;
+using MathNet.Numerics.Random;
 using RefraSin.Numerics.Exceptions;
 using RefraSin.Numerics.RootFinding;
 using RefraSin.TEPSolver.StepVectors;
@@ -10,17 +11,19 @@ namespace RefraSin.TEPSolver.RootFinding;
 public class DirectLagrangianRootFinder(
     IRootFinder rootFinder,
     double minDissipation = 1e-2,
-    int maxIterations = 10
+    int maxIterations = 100,
+    int maxRandomScale = 10
 ) : ILagrangianRootFinder
 {
     /// <inheritdoc />
     public StepVector FindRoot(EquationSystem equationSystem, StepVector initialGuess)
     {
         var map = initialGuess.StepVectorMap;
+        var factor = Vector<double>.Build.Dense(initialGuess.Count, 1.0);
 
         for (int i = 0; i < MaxIterations; i++)
         {
-            var solution = RootFinder.FindRoot(Fun, Jac, initialGuess);
+            var solution = RootFinder.FindRoot(Fun, Jac, initialGuess.PointwiseMultiply(factor));
             var step = new StepVector(solution, map);
             var dissipation = equationSystem.Dissipation(step);
             Log.ForContext<DirectLagrangianRootFinder>()
@@ -29,10 +32,12 @@ public class DirectLagrangianRootFinder(
             if (dissipation > MinDissipation)
                 return step;
 
+            Random.Shared.NextDoubles(factor.Storage.AsArray());
+            factor.Multiply(2 * MaxRandomScale, factor);
+            factor.Subtract(MaxRandomScale, factor);
             Log.Information(
-                "Root finding converged to trivial solution. Trying again with larger initial guess."
+                "Root finding converged to trivial solution. Trying again with initial guess randomly scaled."
             );
-            initialGuess *= 2;
         }
 
         throw new NumericException("Failed to converge to non-trivial stationary point.");
@@ -55,6 +60,8 @@ public class DirectLagrangianRootFinder(
     public double MinDissipation { get; } = minDissipation;
 
     public int MaxIterations { get; } = maxIterations;
+
+    public int MaxRandomScale { get; } = maxRandomScale;
 
     /// <inheritdoc />
     public void RegisterWithSolver(SinteringSolver solver) { }
